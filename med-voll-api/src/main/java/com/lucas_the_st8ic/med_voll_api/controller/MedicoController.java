432f06dbcb613a2b57645b80e2f79725b624240d
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-09-01 17:11:38 -0300

    Updates

---
+++ med-voll-api/src/main/java/com/lucas_the_st8ic/med_voll_api/controller/MedicoController.java
@@ -0,0 +1,17 @@
+package com.lucas_the_st8ic.med_voll_api.controller;
+
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cadastro")
+public class MedicoController {
+
+    @PostMapping("/medicos")
+    public void cadastrar (@RequestBody String cep) {
+        System.out.println(cep);
+    }
+}
---
+++ med-voll-api/src/main/java/com/lucas_the_st8ic/med_voll_api/model/Medico.java
@@ -0,0 +1,4 @@
+package com.lucas_the_st8ic.med_voll_api.model;
+
+public class Medico {
+}
