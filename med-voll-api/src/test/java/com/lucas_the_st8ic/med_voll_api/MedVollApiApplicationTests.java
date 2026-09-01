98e9e728b0d39e7878b163203041c2047ffbdf57
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-09-01 11:47:32 -0300

    Initial commit

---
+++ med-voll-api/src/test/java/com/lucas_the_st8ic/med_voll_api/MedVollApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas_the_st8ic.med_voll_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedVollApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
