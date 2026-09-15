cd584bad7e1252a6930daf1bf4a7f3543bdb1338
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-09-15 15:36:52 -0300

    Create ExceptionHandler.java

---
+++ med-voll-api/src/main/java/com/lucas_the_st8ic/med_voll_api/infra/ExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.lucas_the_st8ic.med_voll_api.infra;
+
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionHandler {
+
+    @org.springframework.web.bind
+            .annotation.ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity notFoundException() {
+
+        return ResponseEntity.notFound().build();
+    }
+}
