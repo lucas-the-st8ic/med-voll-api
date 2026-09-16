6c65d6eace000797b249586e86b5c247cccf579d
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-09-16 17:18:55 -0300

    Update ExceptionHandler.java

--- med-voll-api/src/main/java/com/lucas_the_st8ic/med_voll_api/infra/ExceptionHandler.java
+++ med-voll-api/src/main/java/com/lucas_the_st8ic/med_voll_api/infra/ExceptionHandler.java
@@ -2,7 +2,9 @@ package com.lucas_the_st8ic.med_voll_api.infra;
 
 
 import jakarta.persistence.EntityNotFoundException;
+import org.apache.coyote.BadRequestException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
@@ -13,5 +15,12 @@ public class ExceptionHandler {
     public ResponseEntity notFoundException() {
 
         return ResponseEntity.notFound().build();
+    }
+
+    @org.springframework.web.bind
+            .annotation.ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity badRequestException () {
+        
+        return ResponseEntity.badRequest().build();
     }
 }
