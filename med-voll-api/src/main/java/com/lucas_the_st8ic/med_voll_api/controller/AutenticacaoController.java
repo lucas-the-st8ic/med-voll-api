0aa73a89b42c3df75bcaaecb4d0093326d9ed12d
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-09-21 16:50:45 -0300

    Update AutenticacaoController.java

--- med-voll-api/src/main/java/com/lucas_the_st8ic/med_voll_api/controller/AutenticacaoController.java
+++ med-voll-api/src/main/java/com/lucas_the_st8ic/med_voll_api/controller/AutenticacaoController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/login")
+@RequestMapping
 public class AutenticacaoController {
 
 
@@ -22,7 +22,7 @@ public class AutenticacaoController {
 
 
 
-    @PostMapping
+    @PostMapping("/login")
     public ResponseEntity logIn(@RequestBody @Valid
                                 DadosAutenticacao dados) {
 
