f0a66f97ac13f5e0360262fde65c6542cc0a19f7
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-09-25 17:46:21 -0300

    Update SecurityConfigurations.java

--- med-voll-api/src/main/java/com/lucas_the_st8ic/med_voll_api/infra/security/SecurityConfigurations.java
+++ med-voll-api/src/main/java/com/lucas_the_st8ic/med_voll_api/infra/security/SecurityConfigurations.java
@@ -2,6 +2,7 @@ package com.lucas_the_st8ic.med_voll_api.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,12 +17,17 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfigurations {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http)
-            throws Exception
-    {
-        return http.csrf(csrf -> csrf.disable())
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .csrf(csrf -> csrf.disable())
                 .sessionManagement(session ->
-                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                        .anyRequest().authenticated()
+                )
                 .build();
     }
 
